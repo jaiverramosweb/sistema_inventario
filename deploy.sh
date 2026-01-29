7f49350c18db45ca6736942851af1f37f4e03d3c
Jaiver Ramos <jaiver.ramos7942@gmail.com>
2026-01-28 22:51:23 -0500

    nuevo ajuste

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,6 @@ ln -nfs "$SHARED_DIR/storage" "$RELEASE_DIR/admin-back/storage"
 cd "$RELEASE_DIR/admin-back"
 composer install --no-interaction --prefer-dist --optimize-autoloader
 php artisan migrate --force
-php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
