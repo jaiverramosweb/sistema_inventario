22ef0831c1ae3207d71e2743a3d7bcadf6c2a5bd
Jaiver Ramos <jaiver.ramos7942@gmail.com>
2026-02-21 16:12:32 -0500

    se ajusta en la consola para las tareas de segundo plano

--- admin-back/routes/console.php
+++ admin-back/routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/** 
+ * Tareas Programadas para el Inventario
+ */
+// Se ejecuta cada día a medianoche
+Schedule::command('app:product-process-warehouse')->daily();
+
+// Se ejecuta el primer día de cada mes
+Schedule::command('app:command-product-stock-inicial')->monthly();
